32798695393317745b29a11299410f127c6a4754
Stephen Monnet <stephen-monnet@hotmail.com>
2026-05-27 11:28:43 +0200

    v_des ~ cubic(iter)

--- colav/planner.py
+++ colav/planner.py
@@ -360,7 +360,7 @@ class TimeSpaceColav:
             # Decrease desired speed at each iteration to find a plane that admits at least one feasible path
             # Alternative method: self.projector.v_des = (self.speed_factor**discount_power) * self.desired_speed
             # self.projector.v_des = (1 / (1 + 5 * discount_power / self.max_iter)) * self.desired_speed
-            self.projector.v_des = ((1-discount_power / self.max_iter)**2) * self.desired_speed
+            self.projector.v_des = ((1-discount_power / self.max_iter)**3) * self.desired_speed
             logger.info(f"iteration {k+1}/{self.max_iter} | minimum speed = {self.projector.v_des:.1f} (discount power = {discount_power})")
             discount_power += 1
 
