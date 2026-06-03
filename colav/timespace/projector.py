f4c5377de4549d0389cc0780d2fc33d64f9c7232
Stephen Monnet <stephen-monnet@hotmail.com>
2026-06-03 18:01:40 +0200

    temp

--- colav/timespace/projector.py
+++ colav/timespace/projector.py
@@ -162,16 +162,21 @@ class TimeSpaceProjector:
                 projected_vertices_1, times_1, valid_1 = self.delay_planes[0].intersection(obs.robust_geometry or obs.geometry, obs.vertices_velocity, robust=obs.robust_geometry is not None)
                 projected_vertices_2, times_2, valid_2 = self.delay_planes[1].intersection(obs.robust_geometry or obs.geometry, obs.vertices_velocity, robust=obs.robust_geometry is not None)
 
-                if not(valid_1) and valid_2:
-                    poly1 = Polygon(obs.geometry)
-                    valid_1 = True
+                # if not(valid_1) and valid_2:
+                #     poly1 = Polygon(obs.geometry)
+                #     valid_1 = True
 
-                # If at least one intersection occurs in the future, obstacle is valid
-                if valid_1 and valid_2:
+                # # If at least one intersection occurs in the future, obstacle is valid
+                # if valid_1 and valid_2:
+                if True:
                     poly1 = Polygon(projected_vertices_1)
                     poly2 = Polygon(projected_vertices_2)
                     union_geom = unary_union([poly1, poly2])
                     projected_obstacles.append(union_geom.convex_hull)
+
+                # if obs.mmsi == 219230000:
+                #     print("VALID: ", valid_1, valid_2)
+                #     print("TIMES: ", times_1, times_2)
 
         else:
             # Project moving obstacles 
